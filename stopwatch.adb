cd488df30c697df1f97a1f1cffc3b11054dcfac0
Christoph Haag <haagch.christoph@googlemail.com>
2011-06-17 00:53:15 +0200

    multiple stopwatches

--- stopwatch.adb
+++ stopwatch.adb
@@ -5,12 +5,12 @@ package body Stopwatch is
    procedure Start is
    begin
       starttime := Ada.Real_Time.Clock;
-      if (state = IDLE) then
+      if ((state = NONE) or (state = IDLE)) then
          state := STARTED;
       else
-         state := ERROR;
          errmsg := new String'("An error occured: Stopwatch was started " &
             "while in state: " & STATES'Image (state));
+         state := ERROR;
       end if;
    end Start;
 
@@ -22,9 +22,9 @@ package body Stopwatch is
       elsif (state = ERROR) then
          return;
       else
-         state := ERROR;
          errmsg := new String'("An error occured: Stopwatch was stopped " &
             "while in state: " & STATES'Image (state));
+         state := ERROR;
       end if;
    end Stop;
 
@@ -39,10 +39,76 @@ package body Stopwatch is
       elsif (state = ERROR) then
          return errmsg.all;
       else
-         errmsg := new String'("An error occured: Stopwatch was stopped " &
-            "while in state: " & STATES'Image (state));
+         errmsg := new String'("An error occured: Time read while in state " &
+            STATES'Image (state));
          return errmsg.all;
       end if;
    end Get_Time_Image;
 
+   function Get_State return STATES is
+   begin
+      return state;
+   end Get_State;
+
+
+
+   procedure Create_Stopwatches (count : Positive) is
+   begin
+      StopWatchArray := new StopWatchArrayType (1 .. count);
+   end Create_Stopwatches;
+
+   procedure Start (index : Positive) is
+   begin
+      StopWatchArray (index).starttime := Ada.Real_Time.Clock;
+      if ((StopWatchArray (index).state = IDLE) or
+            (StopWatchArray (index).state = NONE))
+      then
+         StopWatchArray (index).state := STARTED;
+      else
+         StopWatchArray (index).errmsg := new String'("An error occured: " &
+            "Stopwatch was started while in state: " &
+            STATES'Image (StopWatchArray (index).state));
+         StopWatchArray (index).state := ERROR;
+      end if;
+   end Start;
+
+   procedure Stop (index : Positive) is
+   begin
+      StopWatchArray (index).stoptime := Ada.Real_Time.Clock;
+      if (StopWatchArray (index).state = STARTED) then
+         StopWatchArray (index).state := STOPPED;
+      elsif (StopWatchArray (index).state = ERROR) then
+         return;
+      else
+         StopWatchArray (index).errmsg := new String'("An error occured: " &
+            "Stopwatch was stopped while in state: " &
+            STATES'Image (StopWatchArray (index).state));
+         StopWatchArray (index).state := ERROR;
+      end if;
+   end Stop;
+
+   function Get_Time_Image (index : Positive) return String is
+      timespan : Ada.Real_Time.Time_Span;
+   begin
+      if (StopWatchArray (index).state = STOPPED) then
+         timespan := Ada.Real_Time."-"
+            (StopWatchArray (index).stoptime,
+             StopWatchArray (index).starttime);
+         StopWatchArray (index).state := IDLE;
+         return (Duration'Image (Ada.Real_Time.To_Duration (timespan)) &
+            " seconds");
+      elsif (StopWatchArray (index).state = ERROR) then
+         return StopWatchArray (index).errmsg.all;
+      else
+         StopWatchArray (index).errmsg := new String'("An error occured: " &
+            "Time read while in state: " &
+            STATES'Image (StopWatchArray (index).state));
+         return StopWatchArray (index).errmsg.all;
+      end if;
+   end Get_Time_Image;
+
+   function Get_State (index : Positive) return STATES is
+   begin
+      return StopWatchArray (index).state;
+   end Get_State;
 end Stopwatch;
--- stopwatch.ads
+++ stopwatch.ads
@@ -3,15 +3,31 @@
 with Ada.Real_Time;
 
 package Stopwatch is
+   type STATES is (NONE, IDLE, STARTED, STOPPED, ERROR);
 
    procedure Start;
    procedure Stop;
-
    function Get_Time_Image return String;
+   function Get_State return STATES;
+
+   procedure Create_Stopwatches (count : Positive);
+   function Get_Time_Image (index : Positive) return String;
+   procedure Start (index : Positive);
+   procedure Stop (index : Positive);
+   function Get_State (index : Positive) return STATES;
 
 private
+   --  single stopwatch
    starttime, stoptime : Ada.Real_Time.Time;
-   type STATES is (IDLE, STARTED, STOPPED, ERROR);
-   state : STATES := IDLE;
+   state : STATES := NONE;
    errmsg : access String;
+
+   --  multiple stopwatches
+   type StopWatchInstance is record
+      state : STATES := NONE;
+      starttime, stoptime : Ada.Real_Time.Time;
+      errmsg : access String;
+   end record;
+   type StopWatchArrayType is array (Positive range <>) of StopWatchInstance;
+   StopWatchArray : access StopWatchArrayType;
 end Stopwatch;
